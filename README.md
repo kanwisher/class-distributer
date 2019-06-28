c9cbe6055192ce381fe1f2d9288543094cb8aa1b
David Kanwisher <kanwisher@gmail.com>
2019-06-27 20:53:42 -0400

    Add version 1

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">Welcome to Class Distributer 👋</h1>
+<p>
+  <img src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://twitter.com/kanwisher">
+    <img alt="Twitter: kanwisher" src="https://img.shields.io/twitter/follow/kanwisher.svg?style=social" target="_blank" />
+  </a>
+</p>
+
+> Allows TA/Instructor to update the class repository with an activity solution
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+node class-distributer
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **David Kanwisher**
+
+* Twitter: [@kanwisher](https://twitter.com/kanwisher)
+* Github: [@kanwisher](https://github.com/kanwisher)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ class-distributer.js
@@ -0,0 +1,240 @@
+const config = require("./config.json");
+const { 
+  trilogyDir,
+  classDir,
+  activityRoot
+} = config;
+
+/*
+* Simple-git
+* https://www.npmjs.com/package/simple-git
+*/
+const simpleGit = require('simple-git');
+const trilogyRepo = simpleGit(trilogyDir);
+const classRepo = simpleGit(classDir);
+
+/*
+* Inquirer
+* https://www.npmjs.com/package/inquirer
+*/
+const inquirer = require('inquirer');
+/*
+* fs-extra
+* includes all of fs, plus additional enhancements
+*/
+const fse = require('fs-extra');
+const path = require('path');
+
+/*
+* extra features
+* if no config, make user create one using inquirer
+* if no matching files are found, ask if you would like to open the directory in explorer/finder
+*/
+
+function getFolderNames(parentDir) {
+  return fse.readdirSync(parentDir, { withFileTypes: true })
+  .filter((file) => file.isDirectory())
+  .map((dir) => dir.name);
+}
+
+function getSolutions(parentDir) {
+  return fse.readdirSync(parentDir)
+  .filter((file) => file === 'Solved');
+}
+
+function getUnsolved(parentDir) {
+  return fse.readdirSync(parentDir, { withFileTypes: true})
+  .map((file) => file.name)
+  .filter((fileName) => fileName !== "Solved");
+}
+
+function init() {
+  updateRepos().then(mainMenu);
+}
+
+function mainMenu() {
+  const choices = [
+    {
+      name: 'Push solutions for a single activity',
+      value: 'solved'
+    },
+    {
+      name: 'Push unsolved activities for an entire week',
+      value: 'unsolved'
+    },
+    {
+      name: 'Exit',
+      value: 'exit'
+    }
+  ];
+
+  const question = {
+    type: 'rawlist',
+    name: 'menuChoice',
+    message: 'What would you like to do?',
+    choices
+  };
+  
+  inquirer.prompt([question])
+    .then(({menuChoice}) => {
+      switch (menuChoice) {
+        case 'solved':
+        case 'unsolved':
+          pickWeek(menuChoice);
+          break;
+        case 'exit':
+          exit();
+          break;
+      }
+    });
+}
+
+function exit() {
+  process.exit()
+}
+
+function updateRepos() {
+  return new Promise((resolve, reject) => {
+    console.log('Attempting to update repos');
+    trilogyRepo.pull((err, {summary}) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log('TrilogyRepo Updated: ', summary);
+        classRepo.pull((err, {summary}) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log('ClassRepo Updated: ', summary);
+            resolve();
+          }
+        })
+      }
+    });
+  });
+}
+
+function pickWeek(task) {
+  const action = task === 'unsolved' ? 'Copy unsolved' : 'Explore';
+  const question = {
+    type: 'rawlist',
+    name: 'weekDir',
+    message: `${action} activities for which week?`,
+    choices: getFolderNames(trilogyDir)
+  };
+  
+  inquirer.prompt([question])
+    .then(({weekDir}) => {
+      if (task === 'solved') {
+        pickActivity(weekDir);
+      } else {
+        copyUnsolved(weekDir);
+      }
+    });
+}
+
+function pickActivity(weekDir) {
+  const weekPath = path.join(trilogyDir, weekDir, activityRoot);
+  const question = {
+    type: 'rawlist',
+    name: 'activityDir',
+    message: 'Which activity?',
+    choices: getFolderNames(weekPath)
+  }
+
+  inquirer.prompt([question])
+    .then(({activityDir}) => {
+      copySolutions(weekDir, activityDir)
+    });
+}
+
+function copySolutions(weekDir, activityDir) {
+  const trilogyPath = path.join(trilogyDir, weekDir, activityRoot, activityDir);
+  let classPath = path.join(classDir, weekDir, activityRoot, activityDir);
+  const solutions = getSolutions(trilogyPath);
+  if (solutions.length) {
+    solutions.forEach(file => {
+      const fromPath = path.join(trilogyPath, file);
+      const toPath = path.join(classPath, file);
+      fse.copySync(fromPath, toPath);
+    });
+    console.log(`Copied solutions folder with all contents`);
+    gitMenu(weekDir, activityDir);
+  } else {
+    console.log('No Solutions for this activity');
+    console.log('Returning To Menu');
+    pickActivity(weekDir);
+  }
+}
+
+function copyUnsolved(weekDir) {
+  const trilogyPath = path.join(trilogyDir, weekDir, activityRoot);
+  const classPath = path.join(classDir, weekDir, activityRoot);
+  const folders = getFolderNames(trilogyPath);
+  folders.forEach((folderName) => {
+    const folderPath = path.join(trilogyPath, folderName);
+    const unsolved = getUnsolved(folderPath);
+    unsolved.forEach((unsolvedName) => {
+      const fromPath = path.join(folderPath, unsolvedName);
+      const toPath = path.join(classPath, folderName, unsolvedName);
+      fse.copySync(fromPath, toPath);
+    });
+    addCommitPush(weekDir);
+  });
+
+
+}
+
+function gitMenu(weekDir, activityDir) {
+  classRepo.status((err, { not_added: notAdded }) => {
+    if (notAdded.length) {
+      console.log(notAdded);
+
+      const question = {
+        type: 'confirm',
+        name: 'confirmed',
+        default: true,
+        message: 'Would you like to push the above files?'
+      };
+      
+      inquirer.prompt([question])
+      .then(({confirmed}) => {
+        if (confirmed) {
+          addCommitPush(weekDir, activityDir);
+        } else {
+          console.log('Returning to menu');
+          pickActivity(weekDir);
+        }
+      });
+    } else {
+      console.log('Nothing to commit, you must have already pushed these files, returning to menu');
+      pickActivity(weekDir);
+    }
+  });
+}
+
+function addCommitPush(weekDir, activityDir) {
+  let message = `Adding unsolved content for ${weekDir}`;
+  if (activityDir) {
+    message = `Adding solutions for ${activityDir}`;
+  }
+  classRepo.add('.', (err, response) => {
+    if (!err) {;
+      classRepo.commit(message, (err, { summary }) => {
+        if (!err) {
+          classRepo.push((err, result) => {
+            if (!err) {
+              console.log('Content is pushed')
+            }
+            console.log('Returning to menu');
+            updateRepos().then(() => {
+              pickActivity(weekDir);
+            })
+          });
+        }
+      });
+    }
+  });
+}
+
+init();
